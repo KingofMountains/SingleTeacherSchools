f2b9547f5a652eb48d3371fa869799aa083c9a21
Giridharan <n.giridharan@rifluxyss.com>
2017-03-13 16:41:50 +0530

    Done the setup

--- app/src/main/java/com/sts/singleteacherschool/SplashActivity.java
+++ app/src/main/java/com/sts/singleteacherschool/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.sts.singleteacherschool;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            startActivity(new Intent(this,LoginActivity.class));
+        }
     }
 }
