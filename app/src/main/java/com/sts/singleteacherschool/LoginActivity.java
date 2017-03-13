acf90148858f2b89aed1b79d449149eeab7ef320
KingofMountains <dada.girin@gmail.com>
2017-03-14 00:51:03 +0530

    designed form

--- app/src/main/java/com/sts/singleteacherschool/LoginActivity.java
+++ app/src/main/java/com/sts/singleteacherschool/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.sts.singleteacherschool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view) {
+        finish();
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
